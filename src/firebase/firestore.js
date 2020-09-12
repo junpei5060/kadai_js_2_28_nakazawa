349d71dba6a7ba3f569d91cf965042f81809cb5d
junpei5060 <66504532+junpei5060@users.noreply.github.com>
2020-09-12 18:04:02 +0900

    Update firestore.js

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -2,13 +2,7 @@ import Firebase from 'firebase'
 import 'firebase/firestore'
 
 const config = {
-    apiKey: "AIzaSyBEeYBWh1YLhmyWs7o3UriKowuGDBrN5WU",
-    authDomain: "project-memo-17a70.firebaseapp.com",
-    databaseURL: "https://project-memo-17a70.firebaseio.com",
-    projectId: "project-memo-17a70",
-    storageBucket: "project-memo-17a70.appspot.com",
-    messagingSenderId: "774245133492",
-    appId: "1:774245133492:web:8bc71c5e0f5825f95a7be1"
+    
 }
 
 const firebaseApp = Firebase.initializeApp(config, 'exercise-vue')
