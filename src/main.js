fc70201352bac619a1c649e9f7ef90bb9ddcf9ef
junpei5060 <66504532+junpei5060@users.noreply.github.com>
2020-09-12 18:04:31 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -18,13 +18,7 @@ Vue.use(moment)
 Vue.use(BootstrapVue)
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBEeYBWh1YLhmyWs7o3UriKowuGDBrN5WU",
-  authDomain: "project-memo-17a70.firebaseapp.com",
-  databaseURL: "https://project-memo-17a70.firebaseio.com",
-  projectId: "project-memo-17a70",
-  storageBucket: "project-memo-17a70.appspot.com",
-  messagingSenderId: "774245133492",
-  appId: "1:774245133492:web:8bc71c5e0f5825f95a7be1"
+  
 };
 
 firebase.initializeApp(firebaseConfig);
